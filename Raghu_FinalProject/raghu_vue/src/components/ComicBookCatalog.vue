3f244c9d8bf076cb8b1e67389414e15b8fd5d813
raghu ram bogineni <raghurambogineni@gmail.com>
2024-12-01 17:57:59 -0500

    update files

---
+++ Raghu_FinalProject/raghu_vue/src/components/ComicBookCatalog.vue
@@ -0,0 +1,28 @@
+<template>
+    <div v-for="(book, index) of bookList" :key="book.comicBook_title">
+        <!-- Send color by matching index -->
+        <ComicBook :book = book :color="colors[index % colors.length]" />
+    </div>
+</template>
+
+<script>
+    import ComicBook from "./ComicBook.vue";
+
+    export default{
+        name:"ComicBookCatalog",
+        components:{
+           ComicBook
+        },
+        props:{
+            bookList: Array
+        },
+        data() {
+        return {
+            colors: ["#ff746c", "#4781cb", "#8e6903", "#49636c", "#f4c445"]
+        };
+        }
+    }
+</script>
+
+<style scoped>
+</style>
